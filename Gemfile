0c47d3efcaa5513fd2dc73c0bd5b1dec5234e1a2
SHERRY <swasieko@gmail.com>
2017-01-28 22:59:36 +0000

    Implement home page

--- Gemfile
+++ Gemfile
@@ -34,6 +34,9 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
   gem 'rspec-rails', '3.5.1'
+  
+  gem 'guard', '~>2.14.0'
+  gem 'guard-rspec', '~> 4.7.2'
 end
 
 group :development do
--- spec/features/creating_homepage_spec.rb
+++ spec/features/creating_homepage_spec.rb
@@ -1,4 +1,4 @@
-requaire 'rails_helper'
+require 'rails_helper'
 
 RSpec.feature "Creating Home Page" do
   scenario do
@@ -6,7 +6,7 @@ RSpec.feature "Creating Home Page" do
      
      expect(page).to have_link('Home')
      expect(page).to have_link ("Athlete's Den")
-     expect(page).to have_contet("Workout Lounge")
-     expect(page).to have_conetent("Show off your workout")
+     expect(page).to have_content("Workout Lounge")
+     expect(page).to have_content("Show off your workout")
     end
 end
