e89ea634047befd4d2ff3703eb766bf918a3e85b
SHERRY <swasieko@gmail.com>
2017-02-07 16:32:58 +0000

    Ensure only last 7 days workout shows

--- spec/features/exercises/listing_exercises_spec.rb
+++ spec/features/exercises/listing_exercises_spec.rb
@@ -11,6 +11,9 @@ RSpec.feature "Listing Exercises" do
     @e2 = @john.exercises.create(duration_in_min: 55,
                                 workout: "Weight Lifting",
                                 workout_date: 2.days.ago)
+    @e3 = @john.exercises.create(duration_in_min: 35,
+                                workout: "Treadmill",
+                                workout_date: 8.days.ago)
   end
   
   scenario "shows user's workout for last 7 days" do
@@ -26,9 +29,9 @@ RSpec.feature "Listing Exercises" do
     expect(page).to have_content(@e2.workout)
     expect(page).to have_content(@e2.workout_date)
     
-    # expect(page).not_to have_content(@e3.duration_in_min)
-    # expect(page).not_to have_content(@e3.workout)
-    # expect(page).not_to have_content(@e3.workout_date)
+    expect(page).not_to have_content(@e3.duration_in_min)
+    expect(page).not_to have_content(@e3.workout)
+    expect(page).not_to have_content(@e3.workout_date)
     
   end
   
