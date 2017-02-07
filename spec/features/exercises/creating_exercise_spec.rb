45f1aa6e6502af467050b6f9ee326a0577523a36
SHERRY <swasieko@gmail.com>
2017-02-07 19:13:19 +0000

    Fixed broken specs

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "Creating Exercise" do
   before do
-    @john = User.create(email: "john@example.com",
+    @john = User.create(first_name: "John", last_name: "Doe", email: "john@example.com",
                         password: "password")
     login_as(@john)
     
@@ -16,6 +16,7 @@ RSpec.feature "Creating Exercise" do
   end
   
   scenario "with valid inputs" do
+    
    
     fill_in "Duration", with: 70
     fill_in "Workout details", with: "Weight Lifting"
