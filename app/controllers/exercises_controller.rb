703bc8dea6fdeb17ff555f848c705c8f386ae8ba
SHERRY <swasieko@gmail.com>
2017-02-07 17:54:28 +0000

    Implement destroy actions

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,10 +1,12 @@
 class ExercisesController < ApplicationController
+ before_action :set_exercise, only: [:show, :edit, :update, :destroy]
+ 
  def index
    @exercises = current_user.exercises
  end
  
  def show
-  @exercise = current_user.exercises.find params[:id]
+  
  end
  
  def new
@@ -25,11 +27,11 @@ class ExercisesController < ApplicationController
  end
  
  def edit
-   @exercise = current_user.exercises.find params[:id]
+  
  end
  
  def update
-   @exercise = current_user.exercises.find params[:id]
+   
    if @exercise.update(exercise_params)
     flash[:notice] = "Exercise has been updated"
     redirect_to [current_user, @exercise]
@@ -39,7 +41,18 @@ class ExercisesController < ApplicationController
    end
  end
  
+ def destroy
+  
+  @exercise.destroy
+  flash[:notice] = "Exercise has been deleted"
+  redirect_to user_exercises_path(current_user)
+ end
+ 
  private
+ 
+ def set_exercise
+  @exercise = current_user.exercises.find params[:id]
+ end
  
  def exercise_params
   params.require(:exercise).permit(:duration_in_min, :workout, :workout_date, :user_id)
