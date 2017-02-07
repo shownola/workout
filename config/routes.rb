31a4e98ba621c34c499dc53390630e74a7e14747
SHERRY <swasieko@gmail.com>
2017-02-07 00:23:02 +0000

    Add routes, index and new actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'dashboards#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :exercises
+  end
+
 end
